5d372a16b827eef30de026fe45286ba835955fad
lvillalobos <lvillalobos@expediagroup.com>
2022-09-12 09:28:51 -0700

    fix: Remove prints

--- querybook/server/lib/query_executor/all_executors.py
+++ querybook/server/lib/query_executor/all_executors.py
@@ -30,8 +30,6 @@ ALL_EXECUTORS = (
 
 def get_executor_class(language: str, name: str):
     for executor in ALL_EXECUTORS:
-        print(f"Executor: {executor}")
-        print(f"Executor match: {language}, {name}")
         if executor.match(language, name):
             return executor
     raise ValueError(f"Unknown executor {name} with language {language}")
