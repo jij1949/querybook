4c93c4adb74c2af5cb78de16113a46ff4ab037cc
Dave Bauman <dbauman@expediagroup.com>
2022-10-31 13:29:10 -0400

    feat: Enable SlackNotifier plugin

--- plugins/notifier_plugin/__init__.py
+++ plugins/notifier_plugin/__init__.py
@@ -1,1 +1,4 @@
-ALL_PLUGIN_NOTIFIERS = []
+from lib.notify.notifier.email_notifier import EmailNotifier
+from lib.notify.notifier.slack_notifier import SlackNotifier
+
+ALL_PLUGIN_NOTIFIERS = [EmailNotifier(), SlackNotifier()]
