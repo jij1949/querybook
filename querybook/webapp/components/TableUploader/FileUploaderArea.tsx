9f00c104f31f5f4e5131f8d180fb0cfd1efb8c1f
Julian Rojas Valvo <jvalvo@expediagroup.com>
2023-05-02 13:44:53 -0700

    feat: Added Note about file size limit in table upload window (#129)
    * feat: Added Note about file size limit in table upload window
    
    * feat: removed changes from other branch

--- querybook/webapp/components/TableUploader/FileUploaderArea.tsx
+++ querybook/webapp/components/TableUploader/FileUploaderArea.tsx
@@ -96,6 +96,7 @@ export const FileUploaderArea: React.FC<IFileUploaderAreaProps> = ({
                     <StyledText untitled>
                         Drag a file here to upload (Allowed types:{' '}
                         {AllowedFileTypes})
+                        <div>Note: Files should be under 200MB</div>
                     </StyledText>
                 </div>
 
