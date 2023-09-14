0573e1000d47d85e54a1bc8a9e4f2590121a6c2f
Dave Bauman <dbauman@expediagroup.com>
2023-09-14 17:28:10 -0400

    fix: Load OPENAI_API_KEY/BASE from config to environment variable

--- plugins/ai_assistant_plugin/__init__.py
+++ plugins/ai_assistant_plugin/__init__.py
@@ -1,4 +1,12 @@
+import os
+from env import get_env_config
+
 from lib.ai_assistant.assistants.openai_assistant import OpenAIAssistant
+
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = get_env_config("OPENAI_API_KEY")
+if "OPENAI_API_BASE" not in os.environ:
+    os.environ["OPENAI_API_BASE"] = get_env_config("OPENAI_API_BASE")
 
 ALL_PLUGIN_AI_ASSISTANTS = [OpenAIAssistant()]
 
