b87e190525623a6c9054102a6d4eb3cc444297bb
Dave Bauman <dbauman@expediagroup.com>
2023-11-09 15:47:14 -0500

    feat: Improve EG's metastore sync with owners, tags

--- plugins/metastore_plugin/eg_hive_metastore/eg_hive_metastore_loader.py
+++ plugins/metastore_plugin/eg_hive_metastore/eg_hive_metastore_loader.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import List, Tuple
 
 from lib.logger import get_logger
@@ -12,7 +13,14 @@ from lib.metastore.loaders.hive_metastore_loader import (
 )
 from lib.utils import json as ujson
 
-from const.metastore import DataTag, MetastoreLoaderConfig, MetadataType, MetadataMode
+from const.metastore import (
+    DataOwner,
+    DataOwnerType,
+    DataTag,
+    MetastoreLoaderConfig,
+    MetadataType,
+    MetadataMode,
+)
 
 from metastore_plugin.eg_hive_metastore.data_elements import (
     data_elements,
@@ -26,6 +34,58 @@ from const.data_element import (
 
 LOG = get_logger(__file__)
 
+
+class EgTagColors(Enum):
+    SENSITIVITY: str = "#85d0ce"  # icy blue
+    CLOVERLEAF: str = "#ffca00"  # gold
+    GOVERNANCE: str = "#35b5bb"  # blue
+    ICEBERG: str = "#529dce"  # picton blue
+
+
+# See: https://confluence.expedia.biz/display/EDMG/EDMG+-+Tags+for+Data+Governance
+DATASET_TAGS = [
+    {"name": "eg-owner", "mandatory": True, "tag": False},
+    {"name": "eg-creator", "mandatory": True, "tag": False},
+    {
+        "name": "eg-brand",
+        "label": "Brand",
+        "mandatory": False,
+        "tag": True,
+    },
+    {
+        "name": "eg-domain",
+        "label": "Domain",
+        "mandatory": False,
+        "tag": True,
+    },
+    {
+        "name": "eg-storage-location",
+        "label": "Storage Location",
+        "mandatory": True,
+        "tag": True,
+        "concat": "Storage",
+    },
+    {
+        "name": "eg-origin-location",
+        "label": "Origin Location",
+        "mandatory": True,
+        "tag": True,
+        "concat": "Origin",
+    },
+    {
+        "name": "eg-application-name",
+        "label": "Application",
+        "mandatory": True,
+        "tag": True,
+    },
+    {
+        "name": "eg-partner-data",
+        "label": "Partner Data",
+        "mandatory": False,
+        "tag": True,
+    },
+]
+
 #
 # Expedia-customized version of the HMSMetastoreLoader
 #
@@ -34,14 +94,54 @@ class EgHMSMetastoreLoader(HMSMetastoreLoader):
         {
             MetadataType.TAG: MetadataMode.WRITE_BACK,
             MetadataType.DATA_ELEMENT: MetadataMode.WRITE_BACK,
+            MetadataType.OWNER: MetadataMode.WRITE_BACK,
         }
     )
-    # # Can override this to test a limited set of tables
+
+    @classmethod
+    def get_table_owner_types(cls) -> list[DataOwnerType]:
+        """Return all the owner types the metastore supports.
+        Override this method if loading table owners from metastore is enabled
+        and your metastore supports owner types.
+        E.g.
+        [
+            DataOwnerType(
+                name="CREATOR",
+                display_name="Table Creator",
+                description="Person who created the table",
+            ),
+            DataOwnerType(
+                name="BUSINESS_OWNER",
+                display_name="Owners",
+                description="Person or group who is responsible for business related aspects of the table",
+            ),
+        ]
+        The `display_name` will be rendered as the field label in the detailed table view, which is `Owners` by default.
+        """
+        return [
+            DataOwnerType(
+                name=None, display_name="Owners", description="People who own the table"
+            ),
+            DataOwnerType(
+                name="CREATOR",
+                display_name="Creator",
+                description="Creator of the table, from the `eg-creator` tag",
+            ),
+        ]
+
+    # Can override this to test a limited set of tables
     # def get_all_schema_names(self) -> List[str]:
     #     # dbs = self.hmc.get_all_databases()
     #     # LOG.info("dbs: %s", dbs)
 
-    #     return ["conversation"]
+    #     return [
+    #         "conversation",
+    #         "egdp_test_conversation",
+    #         "project_meso_raw",
+    #         "supply",
+    #         "egdp_prod_content",
+    #         "bexg_etl_test_meso",
+    #     ]
 
     def get_table_and_columns(
         self, schema_name, table_name
@@ -69,7 +169,7 @@ class EgHMSMetastoreLoader(HMSMetastoreLoader):
         table = DataTable(
             name=description.tableName,
             type=description.tableType,
-            owner=description.owner,
+            owners=[],
             table_created_at=description.createTime,
             table_updated_by=parameters.get("last_modified_by"),
             table_updated_at=last_modified_time,
@@ -89,24 +189,86 @@ class EgHMSMetastoreLoader(HMSMetastoreLoader):
             )
         )
 
+        tags = []
+
+        if description.owner and description.owner not in [
+            "cloverleaf",
+            "expedia",
+            "hadoop",
+            "root",
+        ]:
+            table.owners.append(DataOwner(username=description.owner))
+
+        #
+        # Process table parameters
+        #
+
+        # Add a table tag if the table is managed by Cloverleaf
+        if (
+            description.owner == "cloverleaf"
+            or parameters.get("eg-managed-by") == "cloverleaf"
+        ):
+            tags.append(
+                DataTag(
+                    name="Cloverleaf",
+                    description="This table is managed by Cloverleaf",
+                    color=EgTagColors.CLOVERLEAF.value,
+                )
+            )
+
+        # Iceberg!
+        if parameters.get("table_type") == "ICEBERG":
+            tags.append(
+                DataTag(
+                    name="Iceberg",
+                    description="This is an Iceberg table",
+                    color=EgTagColors.ICEBERG.value,
+                )
+            )
+
+        if parameters.get("comment") is not None:
+            # If the table has a comment, then use it as the description
+            table = table._replace(description=parameters.get("comment"))
+        if parameters.get("eg-owner") is not None:
+            table.owners.append(DataOwner(username=parameters.get("eg-owner")))
+        if parameters.get("eg-creator") is not None:
+            table.owners.append(
+                DataOwner(username=parameters.get("eg-creator"), type="CREATOR")
+            )
+
+        # Iterate through the list of dataset tags, filtered to tag: True
+        for tag in [tag for tag in DATASET_TAGS if tag["tag"]]:
+            tagName = tag["name"]
+            if parameters.get(tagName) is not None:
+                tags.append(
+                    DataTag(
+                        name=(
+                            tag["concat"] + ": " + parameters.get(tagName)
+                            if tag.get("concat")
+                            else parameters.get(tagName)
+                        ),
+                        type=tag["label"],
+                        description=f"This table is tagged with `{tagName}`",
+                        color=EgTagColors.GOVERNANCE.value,
+                    )
+                )
+
+        # # Disabled top tier for now
+        # # If parameters contains all 5 mandatory tags, then set Top Tier
+        # if all(
+        #     parameters.get(tag["name"]) is not None
+        #     for tag in DATASET_TAGS
+        #     if tag["mandatory"]
+        # ):
+        #     table = table._replace(golden=True)
+
         # If the table is owned by "hadoop" and the schema_name starts with eps-prod,
         # then reassign the owner as e4b-bedrock. This fixes an issue where it displays Unknown (hadoop) as owner
         if description.owner == "hadoop" and schema_name.startswith("eps_prod"):
-            table = table._replace(owner="e4b-bedrock")
+            table.owners.append(DataOwner(username="e4b-bedrock"))
 
-        # If the table is owned by the "cloverleaf" user, then we can
-        # extract the table description and owner from the table
-        if description.owner == "cloverleaf":
-            parameters = description.parameters
-
-            if parameters.get("eg-creator") is not None:
-                table = table._replace(owner=parameters.get("eg-creator"))
-
-            if parameters.get("comment") is not None:
-                table = table._replace(description=parameters.get("comment"))
-
+        # Look for eg-sensitivity tags and apply them to the table and columns
         new_columns = columns
-
         if any(p.startswith("eg-sensitivity.") for p in parameters):
 
             # Map of eg-sensitivity tags: eg-sensitivity.<column_name> = <sensitivity_tag>
@@ -117,15 +279,12 @@ class EgHMSMetastoreLoader(HMSMetastoreLoader):
                 if key.lower().startswith("eg-sensitivity.")
             }
 
-            table = table._replace(
-                tags=table.tags
-                + [
-                    DataTag(
-                        name="Sensitivity",
-                        description="This table contains sensitivity tags",
-                        color="#85d0ce",
-                    )
-                ]
+            tags.append(
+                DataTag(
+                    name="Sensitivity",
+                    description="This table contains sensitivity tags",
+                    color=EgTagColors.SENSITIVITY.value,
+                )
             )
 
             new_columns = [
@@ -138,6 +297,10 @@ class EgHMSMetastoreLoader(HMSMetastoreLoader):
                 )
                 for col in columns
             ]
+
+        # Update the table with the tags if any
+        if tags:
+            table = table._replace(tags=tags)
 
         return table, new_columns
 
@@ -152,7 +315,15 @@ def apply_sensitivity_tag_and_data_element(col, value):
 
     # Always add the sensitivity tag with the value in lower-case
     col = col._replace(
-        tags=col.tags + [DataTag(name=value.lower(), type="Sensitivity")],
+        tags=col.tags
+        + [
+            DataTag(
+                name=value.lower(),
+                type="Sensitivity",
+                color=EgTagColors.SENSITIVITY.value,
+                description="This column contains a sensitivity tag",
+            )
+        ],
     )
 
     # Look for a data element by sensitivity tag value
