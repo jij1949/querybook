00378fa8778445870f46573388b1d9b934d4b5f8
Dave Bauman <dbauman@expediagroup.com>
2024-02-06 16:06:50 -0500

    feat: Add local embedding configurations

--- plugins/vector_store_plugin/__init__.py
+++ plugins/vector_store_plugin/__init__.py
@@ -1,10 +1,20 @@
-ALL_PLUGIN_VECTOR_STORES = {}
-ALL_PLUGIN_EMBEDDINGS = {}
+# ALL_PLUGIN_VECTOR_STORES = {}
+# ALL_PLUGIN_EMBEDDINGS = {}
 
 # Example to add vector store
 
-# from lib.vector_store.stores.opensearch import OpenSearchVectorStore
-# from langchain.embeddings import OpenAIEmbeddings
+from lib.vector_store.stores.opensearch import OpenSearchVectorStore
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import (
+    HuggingFaceEmbeddings,
+    HuggingFaceBgeEmbeddings,
+)
 
-# ALL_PLUGIN_VECTOR_STORES = {"opensearch": OpenSearchVectorStore}
-# ALL_PLUGIN_EMBEDDINGS = {"openai": OpenAIEmbeddings}
+ALL_PLUGIN_VECTOR_STORES = {"opensearch": OpenSearchVectorStore}
+
+ALL_PLUGIN_EMBEDDINGS = {
+    "openai": OpenAIEmbeddings,
+    # Both of these require `sentence_transformers` package to be installed
+    "bge": HuggingFaceBgeEmbeddings,
+    "sentence_transformers": HuggingFaceEmbeddings,
+}
