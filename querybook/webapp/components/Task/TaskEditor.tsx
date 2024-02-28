ea117ec68bb8fa411a4df0e2c583773ceb190f24
Rishi Chandna <rchandna@expediagroup.com>
2024-02-28 10:49:39 -0600

    fix: pre-commit prettier (#233)

--- querybook/webapp/components/Task/TaskEditor.tsx
+++ querybook/webapp/components/Task/TaskEditor.tsx
@@ -62,7 +62,11 @@ function stringToTypedVal(stringVal: boolean | number | string) {
         return false;
     } else if (!isNaN(Number(stringVal))) {
         return Number(stringVal);
-    } else if ( stringVal === 'null' || stringVal === 'undefined' || stringVal === 'None') {
+    } else if (
+        stringVal === 'null' ||
+        stringVal === 'undefined' ||
+        stringVal === 'None'
+    ) {
         return null;
     } else {
         return stringVal;
@@ -229,7 +233,8 @@ export const TaskEditor: React.FunctionComponent<IProps> = ({
         );
 
         const getKwargPlaceholder = (param: string) =>
-            (registeredTaskParamList?.[values.task]?.[param]) ?? 'Insert value';
+            String(registeredTaskParamList?.[values.task]?.[param]) ??
+            'Insert value';
 
         const kwargsDOM = (
             <div className="TaskEditor-kwargs mt12">
