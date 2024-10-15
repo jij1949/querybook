79a935839bd6bc70c406a1c1f5f071eb2cd61120
Dave Bauman <dbauman@expediagroup.com>
2024-10-15 16:04:54 -0400

    fix: Catch exceptions during celery stats logger

--- querybook/server/lib/celery/celery_stats.py
+++ querybook/server/lib/celery/celery_stats.py
@@ -1,22 +1,30 @@
 import time
 import threading
+from lib.logger import get_logger
 from lib.stats_logger import stats_logger, ACTIVE_WORKERS, ACTIVE_TASKS
+
+LOG = get_logger(__file__)
 
 
 def send_stats_logger_metrics(celery):
     while True:
-        i = celery.control.inspect()
+        try:
+            i = celery.control.inspect()
 
-        active = i.active() or {}
+            active = i.active() or {}
 
-        active_workers = list(active.keys())
-        active_tasks = 0
-        for worker in active_workers:
-            if worker in active:
-                active_tasks += len(active[worker])
+            active_workers = list(active.keys())
+            active_tasks = 0
+            for worker in active_workers:
+                if worker in active:
+                    active_tasks += len(active[worker])
 
-        stats_logger.gauge(ACTIVE_WORKERS, len(active_workers))
-        stats_logger.gauge(ACTIVE_TASKS, active_tasks)
+            stats_logger.gauge(ACTIVE_WORKERS, len(active_workers))
+            stats_logger.gauge(ACTIVE_TASKS, active_tasks)
+
+        except Exception:
+            LOG.exception("Error collecting celery stats")
+
         time.sleep(5)
 
 
