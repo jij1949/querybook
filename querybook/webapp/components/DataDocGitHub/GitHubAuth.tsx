5dbab0c55ef72c49e368912b961c4bbc437a7693
Dave Bauman <dbauman@expediagroup.com>
2025-03-11 15:30:02 -0400

    fix: Add GHEC-specific instructions to the GitHub integration auth page

--- querybook/webapp/components/DataDocGitHub/GitHubAuth.tsx
+++ querybook/webapp/components/DataDocGitHub/GitHubAuth.tsx
@@ -15,12 +15,18 @@ export const GitHubAuth: React.FunctionComponent<IProps> = ({
 }) => (
     <div className="GitHubAuth p20">
         <Icon name="Github" size={64} className="GitHubAuth-icon mb20" />
-        <Message
-            title="Connect to GitHub"
-            message="We need your permission to access your GitHub repositories. Please authorize to enable GitHub features on Querybook."
-            type="info"
-            iconSize={32}
-        />
+        <Message title="Connect to GitHub" type="info" iconSize={32}>
+            <p>
+                We need your permission to access your GitHub repositories.
+                Please authorize to enable GitHub features on Querybook.
+            </p>
+            <br />
+            <p>
+                <strong>Important:</strong> use your GHEC account (
+                <code>&lt;username&gt;_expedia</code>) instead of a personal
+                GitHub account.
+            </p>
+        </Message>
         <AsyncButton
             onClick={onAuthorize}
             title="Connect Now"
